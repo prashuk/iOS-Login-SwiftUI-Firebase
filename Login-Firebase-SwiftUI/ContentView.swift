33bd62d837dbd21c9ee440ee7662d1f8ffd07ba5
Prashuk <prashuk.ajmera@gmail.com>
2020-10-26 14:46:34 -0500

    initial commit

---
+++ Login-Firebase-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login-Firebase-SwiftUI
+//
+//  Created by Prashuk Ajmera on 10/25/20.
+//
+
+import SwiftUI
+import Firebase
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Login-Firebase-SwiftUI' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Firebase/Auth'
+
+  # Pods for Login-Firebase-SwiftUI
+
+end
